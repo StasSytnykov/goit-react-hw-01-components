9b49b8adf6d18cae53c2821cb5feaef30b2308b0
Stas Sytnykov <stas82kak05@gmail.com>
2022-04-29 16:35:22 +0300

    completed task 4 without style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,11 @@ import { Container } from './Container';
 import { Profile } from './Profile';
 import { Statistics } from './Statistics';
 import { FriendList } from './FriendList';
+import { TransactionHistory } from './TransactionHistory';
 import user from './Profile/user.json';
 import data from './Statistics/data.json';
 import friends from './FriendList/friends.json';
+import transactions from './TransactionHistory/transactions.json';
 
 export const App = () => {
   return (
@@ -18,6 +20,7 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
+      <TransactionHistory items={transactions} />
     </Container>
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import style from './TransactionHistory.module.css';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className={style.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => {
+          return (
+            <tr key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.object),
+};
